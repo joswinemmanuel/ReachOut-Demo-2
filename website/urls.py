683147676708ac0adfcc855b3b53a11fd91ee4e6
joswinemmanuel <joswinemmanuel2002@gmail.com>
2024-04-14 16:06:44 +0530

    Commit 1

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("assistance/", views.assistance, name="assistance"),
+    path("contact/", views.contact, name="contact"),
+    path("team/", views.team, name="team"),
+    path("testimonial/", views.testimonial, name="testimonial"),
+    path("404/", views.Error404, name="404"),
+]
---
+++ website/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def assistance(request):
+    return render(request, 'assistance.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
+def Error404(request):
+    return render(request, '404.html')
